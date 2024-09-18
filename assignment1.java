265d219ca4cdaca95d86e9675dfac745b34ac5e6
Nagesh1215 <morenagesh106@gmail.com>
2024-09-18 10:45:31 +0530

    Add files via upload

---
+++ assignment1.java
@@ -0,0 +1,11 @@
+
+public class assignment1 {
+
+    public static void main(String args[]) 
+    {
+        for(char i='A'; i<='Z';i++)
+        {
+          System.out.print(i+" ");
+        }
+    }
+}
---
+++ assignment2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class assignment2
+{
+    public static void main(String[] args) 
+    {
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Enter a String:");
+        String input = scanner.nextLine();
+        System.out.println("Vowels in the string:");
+        for(int i=0;i<input.length();i++)
+        {
+            char ch = input.charAt(i);
+            if(ch=='a'|| ch=='e'|| ch=='i' || ch=='o'|| ch=='u' || ch=='A'|| ch=='E' || ch=='I' || ch=='O' || ch=='U')
+            {
+                System.out.println(ch+" ");
+            }
+        }
+
+
+    }
+}
+
